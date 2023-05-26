8528b6559a5a797fd53a93c9c4b98f73d1614092
Scott Harris <31547916+ScottHarris17@users.noreply.github.com>
2023-05-26 15:52:08 -0700

    Added basic analyses

--- Analysis/MatlabHelperFunctions/parseExperiment.m
+++ Analysis/MatlabHelperFunctions/parseExperiment.m
@@ -2,23 +2,23 @@ function [eyeTrace,eyeTimes, stimulusVelocity, stimulusTimes] = parseExperiment(
 %PARSEEXPERIMENT extracts data from text files (saved from unity)
 
 %load the eye trace
-fID = fopen(fullfile(experimentFolderPath, ['gazeSpace-' experimentName '.txt']), 'r');
+fID = fopen(fullfile(experimentFolderPath, 'gazeSpace.txt'), 'r');
 eyeTrace = fscanf(fID, '(%f, %f, %f)\n');
 
 %load the eyeTimes
-fID = fopen(fullfile(experimentFolderPath, ['gazeTime-' experimentName '.txt']), 'r');
+fID = fopen(fullfile(experimentFolderPath, 'gazeTime.txt'), 'r');
 eyeTimes = fscanf(fID, '%f'); %seconds
 
 % load the stimulus absolute timestamps
-% fID = fopen(['gratingAbsTime-' experimentName, '.txt'], 'r');
+% fID = fopen('gratingAbsTime.txt', 'r');
 % stimulusAbsTimestamps = fscanf(fID, '%f');
 
 %load the stimulus oscillation
-fID = fopen(fullfile(experimentFolderPath, ['gratingSpace-' experimentName, '.txt']), 'r');
+fID = fopen(fullfile(experimentFolderPath, 'gratingSpace.txt'), 'r');
 stimulusVelocity = fscanf(fID, '%f');
 
 %load the stimulus timestamps
-fID = fopen(fullfile(experimentFolderPath, ['gratingTime-' experimentName, '.txt']), 'r');
+fID = fopen(fullfile(experimentFolderPath, 'gratingTime.txt'), 'r');
 stimulusTimes = fscanf(fID, '%f');
 end
 
